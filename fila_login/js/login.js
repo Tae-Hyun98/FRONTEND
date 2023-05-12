5b19072f554d840578bba740ed3dbdb7b854f45a
TaeHyun <kt9811@naver.com>
2023-05-12 15:41:16 +0900

    fila-login

--- fila_login/js/login.js
+++ fila_login/js/login.js
@@ -1,6 +1,9 @@
+import userData from "./login_data.js";
+
 //눈표시누르면 input type -> text 끄면 input type -> password
 const loginId = document.getElementById('login_id');
 const loginPw = document.getElementById('login_pw');
+const loninBtn = document.getElementById('login_btn');
 
 const eye = document.querySelector('.eye');
 const eyeOff = document.querySelector('.eye_off');
@@ -19,3 +22,13 @@ function inputPassword() {
   eye.style.display = 'block';
   eyeOff.style.display = 'none';
 }
+
+loninBtn.addEventListener('click', () => {
+  for (let i = 0; i < userData.length; i++) {
+    if (loginId.value === userData[i].userid && loginPw.value === userData[i].password) {
+      alert('로그인성공');
+    } else {
+      alert('회원가입해주세요');
+    }
+  }
+});
---
+++ fila_login/js/login_data.js
@@ -0,0 +1,9 @@
+const userData = [
+  {
+  id: 'data01',
+  userid:'taehyun12',
+  password: '12345@',
+},
+]
+
+export default userData;
