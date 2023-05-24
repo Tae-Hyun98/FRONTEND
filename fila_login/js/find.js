e67b42676f060c0000fdb7d20fd77e7eca5a7d88
TaeHyun <kt9811@naver.com>
2023-05-24 12:09:49 +0900

    login

--- fila_login/js/find.js
+++ fila_login/js/find.js
@@ -19,30 +19,45 @@ findPwBtn.addEventListener('click', clickPwBtn);
 //아이디찾기 버튼
 function clickBtn() {
   if (findName.value !== '' && idcardFront.value !== '' && idcardBack.value !== '') {
-    for (let i = 0; i < userData.length; i++) {
-      if (findName.value === userData[i].username && idcardFront.value === userData[i].idcard_front && idcardBack.value === userData[i].idcard_back) {
-        alert('회원님의 아이디는' + userData[i].userid + '입니다.');
-        break;
-      } 
-    }
+    userData.forEach(vl => {
+      if (findName.value === vl.username && idcardFront.value === vl.idcard_front && idcardBack.value === vl.idcard_back) {
+        alert(`회원님의 아이디는 ${vl.userid} 입니다`);
+      } /* else if (findName.value !== vl.username || idcardFront.value !== vl.idcard_front || idcardBack.value !== vl.idcard_back) {
+        alert('다시 입력해주세요');
+      } */
+    });
   } else {
     alert('정보를 입력해주세요.');
   }
 }
 
+
 //비밀번호 찾기
 function clickPwBtn() {
   if (userId.value !== '' && userName.value !== '' && idcardFrontPw.value !== '' && idcardBackPw.value !== '') {
     for (let i = 0; i < userData.length; i++) {
-      if (userId.value === userData[i].userid && userName.value === userData[i].username && idcardFrontPw.value === userData[i].idcard_front && idcardBackPw.value === userData[i].idcard_back) {
+      if ((userId.value === userData[i].userid) && (userName.value === userData[i].username) && (idcardFrontPw.value === userData[i].idcard_front) && (idcardBackPw.value === userData[i].idcard_back)) {
         alert('회원님의 비밀번호는 : ' + userData[i].password + '입니다.');
-        break;
-      } else {
-        alert('정보를 확인해주세요');
-        break;
       }
+      /* else if ((userId.value !== userData[i].userid) || (userName.value !== userData[i].username) || (idcardFrontPw.value !== userData[i].idcard_front) || (idcardBackPw.value !== userData[i].idcard_back)) {
+             alert('정보를 확인해주세요');
+             break;
+           } */
     }
   } else {
     alert('정보를 입력해주세요');
   }
 }
+
+
+//검색버튼 클릭시 검색창
+const scr = document.querySelector('body');
+const searchOpen = document.getElementById('search');
+const searchClose = document.getElementById('close');
+
+searchOpen.addEventListener('click', () => {
+  scr.classList.add('search_on');
+});
+searchClose.addEventListener('click', () => {
+  scr.classList.remove('search_on');
+});
--- fila_login/js/login.js
+++ fila_login/js/login.js
@@ -1,5 +1,18 @@
 import userData from "./login_data.js";
 
+//검색버튼 클릭시 검색창
+const scr = document.querySelector('body');
+const searchOpen = document.getElementById('search');
+const searchClose = document.getElementById('close');
+
+searchOpen.addEventListener('click', () => {
+  scr.classList.add('search_on');
+});
+searchClose.addEventListener('click', () => {
+  scr.classList.remove('search_on');
+});
+
+
 //눈표시누르면 input type -> text 끄면 input type -> password
 const loginId = document.getElementById('login_id');
 const loginPw = document.getElementById('login_pw');
@@ -25,15 +38,13 @@ function inputPassword() {
 
 loninBtn.addEventListener('click', () => {
   for (let i = 0; i < userData.length; i++) {
-    if ((loginId.value !== '' && loginPw.value !== '') && (loginId.value === userData[i].userid || loginPw.value === userData[i].password)) {
+    if ((loginId.value !== '' && loginPw.value !== '') || (loginId.value === userData[i].userid || loginPw.value === userData[i].password)) {
       if (loginId.value === userData[i].userid && loginPw.value === userData[i].password) {
         alert('로그인성공');
       } else if (loginId.value !== userData[i].userid && loginPw.value === userData[i].password) {
         alert('아이디가 틀렸습니다.');
       } else if (loginId.value === userData[i].userid && loginPw.value !== userData[i].password) {
         alert('비밀번호가 틀렸습니다.');
-      } else if (loginId.value !== userData[i].userid && loginPw.value !== userData[i].password) {
-        alert('일치하는 정보 없음');
       }
     }
   }
